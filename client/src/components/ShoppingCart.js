e7f90f13dbaab6db883f8d9916a833eebfe52066
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-09-04 14:03:46 -0400

    Could not finish the stretch in time

--- client/src/components/ShoppingCart.js
+++ client/src/components/ShoppingCart.js
@@ -5,12 +5,12 @@ import React from "react";
 export default function ShoppingCart(props) {
   const total = props.cart.reduce((sum, d) => sum + d.price, 0);
   return (
-    <div className="cart">
+    <div className="cart" data-testid="testName">
       {props.cart.map((plant) => (
-        <div className="plant-card" key={plant.id}>
+        <div className="plant-card" key={plant.id} >
           <img className="plant-image" src={plant.img} alt={plant.name} />
           <div className="plant-details">
-            <h2 className="plant-name">{plant.name}</h2>
+            <h2 className="plant-name" >{plant.name}</h2>
             <p>${plant.price}</p>
             <button
               className="plant-button"
