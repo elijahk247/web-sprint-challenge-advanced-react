429dcf9949fd4c3930d2f5e66b50769f4e37b0a2
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-09-04 12:50:28 -0400

    Have not added the handleSubmit to useForm yet; unsure if i will. Working on the testings and finished the first test, working on the second

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,20 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  // arrange
+  const container = render(<CheckoutForm />);
+  console.log(container)
+
+  // act
+  const header = screen.getByText(/checkout form/i)
+  console.log(header)
+
+  //assert
+  expect(header).toBeInTheDocument()
+});
 
 test("form shows success message on submit with form details", () => {});
